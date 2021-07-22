ca9fae868ff111ccf08fb7c77eb7e3c25ac1c349
onuo77 <onuo77@gmail.com>
2021-07-23 00:26:14 +0900

    ADD Movie, Reservation, ReservationTicket, screening VO / 공통파일 일부 주석삭제

--- src/main/java/com/cinemabox/web/controller/HomeController.java
+++ src/main/java/com/cinemabox/web/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController{
 	
 	@GetMapping(path = {"/", "/home"})
 	public String home() {
-		return "home";	// /WEB-INF/views/home.jsp 경로에서 "/WEB-INF/views"와 ".jsp"를 제외한 이름
+		return "home";	
 	}
 
 }
