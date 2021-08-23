f527d9559098d6aa3773df4b591859aada03eb02
onuo77 <onuo77@gmail.com>
2021-08-23 14:27:02 +0900

    MODIFY 홈컨트롤러 movieDao삭제

--- src/main/java/com/cinemabox/web/controller/HomeController.java
+++ src/main/java/com/cinemabox/web/controller/HomeController.java
@@ -7,32 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cinemabox.dao.movie.MovieDao;
-import com.cinemabox.dto.Notice.NoticeDto;
 import com.cinemabox.service.movie.MovieService;
 import com.cinemabox.service.theater.Notice.NoticeService;
 import com.cinemabox.vo.Movie;
 import com.cinemabox.vo.Notice;
-import com.cinemabox.web.utils.SessionUtils;
 
 @Controller
 public class HomeController{
 	@Autowired
 	NoticeService noticeService;
 	@Autowired 
-	MovieDao movieDao;
-	@Autowired 
 	MovieService movieService;
 	
 	@GetMapping(path = {"/", "/home"})
 	public String home(Model model, String sort) {
-		//검색 박스오피스
-		List<Movie> nowMovies = movieDao.getNowMovieList("ticket");
-		SessionUtils.addAttribute("NOW_MOIVES", nowMovies);
-		
 		//박스오피스 뿌리기
 		List<Movie> boxOfficeMovies = movieService.getNowMovieList(sort);
-		model.addAttribute("nowMovies", nowMovies);
+		model.addAttribute("nowMovies", boxOfficeMovies);
 		
 		//이벤트 뿌리기
 		
